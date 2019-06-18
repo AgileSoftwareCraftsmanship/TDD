6c685404a5faafeec466317438084801d44c2a13
Lenovo <Lenovo@DESKTOP-ECG9JSB>
2019-06-18 10:16:46 +0300

    First project setup

---
+++ Fraction/src/main/java/org/agilesoftwarecraftmanship/test/Fraction/App.java
@@ -0,0 +1,13 @@
+package org.agilesoftwarecraftmanship.test.Fraction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
